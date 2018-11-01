d4591d3efa58eecf60f60a344d0cbda70d0a5659
lakshmi narayana <b.lak.narayana@gmail.com>
2018-11-01 09:35:54 +0530

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { MobileRechargeAPIService } from './mobile-recharge-api.service';
 import { HeaderComponent } from './header/header.component';
+import { ShowHistoryComponent } from './show-history/show-history.component';
+import { GridComponent } from './show-history/grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MobileComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShowHistoryComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
